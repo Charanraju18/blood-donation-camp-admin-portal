c8a8e6028ec6819b86480f03119132f97d0b0ce9
Charanraju18 <charanraju925@gmail.com>
2024-10-21 06:57:09 +0530

    getimages page done

--- src/GetImages.jsx
+++ src/GetImages.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React from "react";
+import Images from "./components/Images";
+import Leftside_Div from './components/Leftside_Div';
+import './GetImages.css'
 export default function GetImages() {
   return (
-    <h1>Galary page</h1>
-  )
+    <>
+      <div className="outer_container">
+        <Leftside_Div/>
+        <div className="images_container">
+          {
+            Images.map((img, ind) => (
+              <div key={ind} className="img_div">
+                <img src={img} alt={`Image-${ind + 1}`} className="images"/>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,14 @@
+const Images = [
+	"https://th.bing.com/th/id/OIP.To6_861vuLE4Q4PKV9Rv4gHaEK?w=298&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP._RDkMqu-GRb25ygZJMG2zwHaEe?w=276&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://sdmit.in/wp-content/uploads/2019/04/Blood-donation_1-min.jpg",
+	"https://www.ewubd.edu/storage/app/uploads/public/642/150/bac/642150bac35f9583636577.jpg",
+	"https://th.bing.com/th/id/OIP.FOvRjss8xstKIBZgyiDjHAHaGG?w=199&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP.L2w3zqjIwvQkDuRH-UZjlQHaFj?w=225&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP.7K2mPHK9Ho1dse0S53lokQHaE5?w=255&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP.G7RgmLX5DnuNmEey7s84fgHaFj?w=226&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP.wfGBBVaXWUNTxVgDQvmF7gHaE8?w=254&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+	"https://th.bing.com/th/id/OIP.m6KyIOizOG91et8i3sxhVgHaE8?w=260&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+]
+
+export default Images
